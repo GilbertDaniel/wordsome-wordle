c708e108875bbd25d6aa330328143f657369dd44
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-16 20:43:17 +0630

    Setting up BoardComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import './App.css'
+import Board from './components/Board'
+import Keyboard from './components/Keyboard'
+import { boardDefault } from './Words'
+
+export const AppContext = createContext()
+
 const App = () => {
+  const [board, setBoard] = useState(boardDefault)
   return (
-    <div>App</div>
+    <div className='App'>
+      <nav>
+        <h3>WORD SOME - Wordle</h3>
+      </nav>
+      <AppContext.Provider value={{board, setBoard}}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
   )
 }
 
